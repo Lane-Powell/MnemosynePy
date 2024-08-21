8f9fe4fdecbbfd5f6ed4e258a08ddffeb9c906ce
lcp <lane.powell@protonmail.com>
2024-08-21 14:20:09 -0700

    Print default library name on startup and remove unneccessary linebreaks

--- Mnemosyne.py
+++ Mnemosyne.py
@@ -487,21 +487,22 @@ if __name__ == '__main__':
     try:
         current_library = open_library(default_library_name)
     except FileNotFoundError: print('Cannot find default library defined in config.json')
+    print('Default library: {current_library.name} is open.')
 
     status = True
     display = []
 
     # Main loop:
     while status == True:
-        print('\n')
         user_input = input('Instructions: ')
         # Don't accept input if blank:
         if len(user_input) == 0 or user_input.isspace():
             continue
-        print('\n')
+        #print('\n')
         call = call_librarian(display, current_library, user_input)
         status = call[0]
         display = call[1]
         current_library = call[2]
+        #print('\n')
 
     current_library.commit()
